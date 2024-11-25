9f7654bd1d8c719648cf3cc5b33571ab5837c0bb
Avijeet <avijeetpandey25@gmail.com>
2024-11-26 02:35:20 +0530

    add: init code

---
+++ src/main/java/com/express/xpressbackend/XpressBackendApplication.java
@@ -0,0 +1,13 @@
+package com.express.xpressbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpressBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpressBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/express/xpressbackend/XpressBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.express.xpressbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpressBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
